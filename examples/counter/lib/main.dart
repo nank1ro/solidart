b9aafba99a6b25f2ba5579895a29e6c40f4f8293
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2023-01-20 16:34:44 +0100

    derived signals

--- examples/counter/lib/main.dart
+++ examples/counter/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_solidart/flutter_solidart.dart';
 
+// sample User class
+class User {
+  const User({
+    required this.name,
+    required this.age,
+  });
+
+  final String name;
+  final int age;
+
+  User copyWith({
+    String? name,
+    int? age,
+  }) {
+    return User(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+}
+
+void a() {}
+
 void main() {
   runApp(const MyApp());
 }
