4ff91b66ac713974139b7d01bdc1175a6d2f5bb6
Manuel Plavsic <55398763+manuel-plavsic@users.noreply.github.com>
2023-06-12 19:10:41 +0200

    Add wrapped class

--- packages/solidart/lib/src/utils.dart
+++ packages/solidart/lib/src/utils.dart
@@ -60,4 +60,79 @@ class SolidartCaughtException extends SolidartException {
 /// Creates a delayer scheduler with the given [duration].
 Timer Function(void Function()) createDelayedScheduler(Duration duration) =>
     (fn) => Timer(duration, fn);
+
 /// coverage:ignore-end
+
+/// {@template data}
+/// A wrapper class that makes it possible to distinguish
+/// between `null` as valid instance of [T] and `null` as not present.
+/// Especially useful if [T] is nullable.
+/// {@endtemplate}
+@immutable
+class Wrapped<T> {
+  /// {@macro data}
+  const Wrapped(T value) : _value = value;
+
+  final T _value;
+
+  /// Unwraps the data held by the instance. Usage examples:
+  ///
+  /// - If [T] is nullable:
+  ///
+  /// ```dart
+  ///   try {
+  ///     final wrappedData = userResourceState.wrappedData;
+  ///     if (wrappedData == null) {
+  ///       // in loading state
+  ///
+  ///       // do something signalizing we are in loading state
+  ///     } else {
+  ///       // in data state
+  ///
+  ///       final data = wrappedData.unwrap();
+  ///       if (data == null) {
+  ///         // do something with data-is-null case
+  ///       } else {
+  ///         // do something with data
+  ///       }
+  ///     }
+  ///   } catch (e) {
+  ///     // in error state
+  /// 
+  ///     // handle error
+  ///   }
+  /// ```
+  ///
+  /// - If [T] is not nullable:
+  ///
+  ///   ```dart
+  ///   try {
+  ///     final wrappedData = userResourceState.wrappedData;
+  ///     if (wrappedData == null) {
+  ///       // in loading state
+  /// 
+  ///       // do something signalizing we are in loading state
+  ///     } else {
+  ///       // in data state
+  ///
+  ///       final data = wrappedData.unwrap(); // no need to make distinction
+  ///       // do something with data
+  ///     }
+  ///   } catch (e) {
+  ///     // in error state
+  /// 
+  ///     // handle error
+  ///   }
+  ///   ```
+  T get unwrap => _value;
+
+  @override
+  bool operator ==(Object other) =>
+    identical(this, other) ||
+    other is Wrapped &&
+    runtimeType == other.runtimeType &&
+    _value == other._value;
+
+  @override
+  int get hashCode => _value.hashCode;
+}
