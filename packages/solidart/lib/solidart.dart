0cf67060ff6fbed8cfaa60cf98a358479fdb57d3
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2023-06-22 17:13:42 +0200

    chore/hide internal classes/fuctions from solidart

--- packages/solidart/lib/solidart.dart
+++ packages/solidart/lib/solidart.dart
@@ -3,7 +3,16 @@
 /// More dartdocs go here.
 library solidart;
 
-export 'src/core/core.dart' hide Atom, ReactiveConfig, ReactiveContext;
+export 'src/core/core.dart'
+    hide
+        Atom,
+        Derivation,
+        DerivationState,
+        ReactionErrorHandler,
+        ReactionInterface,
+        ReactiveConfig,
+        ReactiveContext,
+        ValueComparator;
 export 'src/extensions.dart';
 export 'src/utils.dart'
     show SolidartCaughtException, SolidartException, SolidartReactionException;
