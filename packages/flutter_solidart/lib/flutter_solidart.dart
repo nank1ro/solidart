dd59e3147f52935ce687e5cd6c20067168b6b48a
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2023-07-02 16:28:26 +0200

    chore: delete signal options

--- packages/flutter_solidart/lib/flutter_solidart.dart
+++ packages/flutter_solidart/lib/flutter_solidart.dart
@@ -5,7 +5,6 @@ export 'package:solidart/solidart.dart';
 
 export 'src/utils/extensions.dart';
 export 'src/utils/solid_extensions.dart';
-export 'src/utils/solid_options.dart';
 export 'src/widgets/resource_builder.dart';
 export 'src/widgets/show.dart';
 export 'src/widgets/signal_builder.dart';
