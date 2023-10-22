659556cd05bb00740a6a68fb2833fce21f7ad28d
Alexandru Mariuti <alex@mariuti.com>
2023-10-22 15:34:21 +0100

    add methods and comments to set

--- packages/solidart/lib/src/core/collections/set.dart
+++ packages/solidart/lib/src/core/collections/set.dart
@@ -88,6 +88,27 @@ class SetSignal<E> extends ReadSignal<Set<E>> with SetMixin<E> {
     return _value.length;
   }
 
+  /// Returns the [index]th element.
+  ///
+  /// The [index] must be non-negative and less than [length].
+  /// Index zero represents the first element (so `iterable.elementAt(0)` is
+  /// equivalent to `iterable.first`).
+  ///
+  /// May iterate through the elements in iteration order, ignoring the
+  /// first [index] elements and then returning the next.
+  /// Some iterables may have a more efficient way to find the element.
+  ///
+  /// Example:
+  /// ```dart
+  /// final numbers = <int>[1, 2, 3, 5, 6, 7];
+  /// final elementAt = numbers.elementAt(4); // 6
+  /// ```
+  @override
+  E elementAt(int index) {
+    _reportObserved();
+    return _value.elementAt(index);
+  }
+
   /// If an object equal to [object] is in the set, return it.
   ///
   /// Checks whether [object] is in the set, like [contains], and if so,
@@ -137,6 +158,43 @@ class SetSignal<E> extends ReadSignal<Set<E>> with SetMixin<E> {
     return didRemove;
   }
 
+  /// Removes each element of [elements] from this set.
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// characters.removeAll({'A', 'B', 'X'});
+  /// print(characters); // {C}
+  /// ```
+  @override
+  void removeAll(Iterable<Object?> elements) {
+    _setPreviousValue(Set.of(_value));
+    var hasChanges = false;
+    for (final element in elements) {
+      final removed = _value.remove(element);
+      if (!hasChanges && removed) {
+        hasChanges = true;
+      }
+    }
+
+    if (hasChanges) _notifyChanged();
+  }
+
+  /// Removes all elements of this set that satisfy [test].
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// characters.removeWhere((element) => element.startsWith('B'));
+  /// print(characters); // {A, C}
+  /// ```
+  @override
+  void removeWhere(bool Function(E element) test) {
+    final toRemove = <E>[];
+    for (final element in this) {
+      if (test(element)) {
+        toRemove.add(element);
+      }
+    }
+    removeAll(toRemove);
+  }
+
   /// Creates a [Set] with the same elements and behavior as this `Set`.
   ///
   /// The returned set behaves the same as this set
@@ -182,6 +240,184 @@ class SetSignal<E> extends ReadSignal<Set<E>> with SetMixin<E> {
       _value.clear();
       _notifyChanged();
     }
+  }
+
+  /// Removes all elements of this set that are not elements in [elements].
+  ///
+  /// Checks for each element of [elements] whether there is an element in this
+  /// set that is equal to it (according to `this.contains`), and if so, the
+  /// equal element in this set is retained, and elements that are not equal
+  /// to any element in [elements] are removed.
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// characters.retainAll({'A', 'B', 'X'});
+  /// print(characters); // {A, B}
+  /// ```
+  @override
+  void retainAll(Iterable<Object?> elements) {
+    // Create a copy of the set, remove all of elements from the copy,
+    // then remove all remaining elements in copy from this.
+    final toRemove = _value.toSet();
+    for (final e in elements) {
+      toRemove.remove(e);
+    }
+    removeAll(toRemove);
+  }
+
+  /// Removes all elements of this set that fail to satisfy [test].
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// characters.retainWhere(
+  ///     (element) => element.startsWith('B') || element.startsWith('C'));
+  /// print(characters); // {B, C}
+  /// ```
+  @override
+  void retainWhere(bool Function(E element) test) {
+    final removed = <E>[];
+    for (var i = _value.length - 1; i >= 0; --i) {
+      final element = _value.elementAt(i);
+      if (!test(element)) {
+        removed.add(element);
+      }
+    }
+    if (removed.isNotEmpty) {
+      _setPreviousValue(Set.of(_value));
+      for (final item in removed) {
+        _value.remove(item);
+      }
+    }
+  }
+
+  /// Creates a [List] containing the elements of this [Iterable].
+  ///
+  /// The elements are in iteration order.
+  /// The list is fixed-length if [growable] is false.
+  ///
+  /// Example:
+  /// ```dart
+  /// final planets = <int, String>{1: 'Mercury', 2: 'Venus', 3: 'Mars'};
+  /// final keysList = planets.keys.toList(growable: false); // [1, 2, 3]
+  /// final valuesList =
+  ///     planets.values.toList(growable: false); // [Mercury, Venus, Mars]
+  /// ```
+  @override
+  List<E> toList({bool growable = true}) {
+    _reportObserved();
+    return _value.toList(growable: growable);
+  }
+
+  /// The last element that satisfies the given predicate [test].
+  ///
+  /// An iterable that can access its elements directly may check its
+  /// elements in any order (for example a list starts by checking the
+  /// last element and then moves towards the start of the list).
+  /// The default implementation iterates elements in iteration order,
+  /// checks `test(element)` for each,
+  /// and finally returns that last one that matched.
+  ///
+  /// Example:
+  /// ```dart
+  /// final numbers = <int>[1, 2, 3, 5, 6, 7];
+  /// var result = numbers.lastWhere((element) => element < 5); // 3
+  /// result = numbers.lastWhere((element) => element > 5); // 7
+  /// result = numbers.lastWhere((element) => element > 10,
+  ///     orElse: () => -1); // -1
+  /// ```
+  ///
+  /// If no element satisfies [test], the result of invoking the [orElse]
+  /// function is returned.
+  /// If [orElse] is omitted, it defaults to throwing a [StateError].
+  @override
+  E lastWhere(bool Function(E value) test, {E Function()? orElse}) {
+    _reportObserved();
+    return _value.lastWhere(test, orElse: orElse);
+  }
+
+  /// The first element that satisfies the given predicate [test].
+  ///
+  /// Iterates through elements and returns the first to satisfy [test].
+  ///
+  /// Example:
+  /// ```dart
+  /// final numbers = <int>[1, 2, 3, 5, 6, 7];
+  /// var result = numbers.firstWhere((element) => element < 5); // 1
+  /// result = numbers.firstWhere((element) => element > 5); // 6
+  /// result =
+  ///     numbers.firstWhere((element) => element > 10, orElse: () => -1); // -1
+  /// ```
+  ///
+  /// If no element satisfies [test], the result of invoking the [orElse]
+  /// function is returned.
+  /// If [orElse] is omitted, it defaults to throwing a [StateError].
+  /// Stops iterating on the first matching element.
+  @override
+  E firstWhere(bool Function(E value) test, {E Function()? orElse}) {
+    _reportObserved();
+    return _value.firstWhere(test, orElse: orElse);
+  }
+
+  /// Checks that this iterable has only one element, and returns that element.
+  ///
+  /// Throws a [StateError] if `this` is empty or has more than one element.
+  /// This operation will not iterate past the second element.
+  @override
+  E get single {
+    _reportObserved();
+    return _value.single;
+  }
+
+  /// The single element that satisfies [test].
+  ///
+  /// Checks elements to see if `test(element)` returns true.
+  /// If exactly one element satisfies [test], that element is returned.
+  /// If more than one matching element is found, throws [StateError].
+  /// If no matching element is found, returns the result of [orElse].
+  /// If [orElse] is omitted, it defaults to throwing a [StateError].
+  ///
+  /// Example:
+  /// ```dart
+  /// final numbers = <int>[2, 2, 10];
+  /// var result = numbers.singleWhere((element) => element > 5); // 10
+  /// ```
+  /// When no matching element is found, the result of calling [orElse] is
+  /// returned instead.
+  /// ```dart continued
+  /// result = numbers.singleWhere((element) => element == 1,
+  ///     orElse: () => -1); // -1
+  /// ```
+  /// There must not be more than one matching element.
+  /// ```dart continued
+  /// result = numbers.singleWhere((element) => element == 2); // Throws Error.
+  /// ```
+  @override
+  E singleWhere(bool Function(E value) test, {E Function()? orElse}) {
+    _reportObserved();
+    return _value.singleWhere(test, orElse: orElse);
+  }
+
+  /// The first element.
+  ///
+  /// Throws a [StateError] if `this` is empty.
+  /// Otherwise returns the first element in the iteration order,
+  /// equivalent to `this.elementAt(0)`.
+  @override
+  E get first {
+    _reportObserved();
+    return _value.first;
+  }
+
+  /// The last element.
+  ///
+  /// Throws a [StateError] if `this` is empty.
+  /// Otherwise may iterate through the elements and returns the last one
+  /// seen.
+  /// Some iterables may have more efficient ways to find the last element
+  /// (for example a list can directly access the last element,
+  /// without iterating through the previous ones).
+  @override
+  E get last {
+    _reportObserved();
+    return _value.last;
   }
 
   void _notifyChanged() {
