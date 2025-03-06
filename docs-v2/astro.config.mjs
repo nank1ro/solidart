985a8e35d6d6c97ce18c5cc77599f3db88105b07
Alexandru Mariuti <alex@mariuti.com>
2025-03-06 17:36:31 +0100

    docs: site map

--- docs-v2/astro.config.mjs
+++ docs-v2/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://solidart.mariuti.com',
   integrations: [
     starlight({
       title: 'solidart docs',
