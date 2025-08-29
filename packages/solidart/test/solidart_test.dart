fd3ac7776dd1c46271d2f33e58fe7b309aa18210
Alexandru Mariuti <alex@mariuti.com>
2025-08-29 16:23:45 +0200

    chore: add unit test for signal name

--- packages/solidart/test/solidart_test.dart
+++ packages/solidart/test/solidart_test.dart
@@ -293,6 +293,11 @@ void main() {
         expect(s.toString(), startsWith('Signal<int>(value: 0'));
       });
 
+      test('check custom name', () {
+        final s = Signal(0, name: 'custom-name');
+        expect(s.name, 'custom-name');
+      });
+
       test('check Signal becomes ReadSignal', () {
         final s = Signal(0);
         expect(s, const TypeMatcher<Signal<int>>());
