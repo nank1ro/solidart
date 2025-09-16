51b52159291bc21df96e4c971019cd9c85a6e5bd
Alexandru Mariuti <alex@mariuti.com>
2025-09-16 15:41:43 +0200

    chore: update example

--- packages/solidart_hooks/solidart_hooks_example/lib/main.dart
+++ packages/solidart_hooks/solidart_hooks_example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:solidart/solidart.dart';
 import 'package:solidart_hooks/solidart_hooks.dart';
 
 void main() {
@@ -18,24 +19,40 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Example extends HookWidget {
+class Example extends StatefulHookWidget {
   const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final existingSignal = Signal(10);
 
   @override
   Widget build(BuildContext context) {
     final count = useSignal(0);
     final doubleCount = useComputed(() => count.value * 2);
+    final existing = useExistingSignal(existingSignal);
     useSolidartEffect(() {
       debugPrint(
-        'Effect count: ${count.value}, doubleCount: ${doubleCount.value}',
+        'Effect count: ${count.value}, doubleCount: ${doubleCount.value}, existing: ${existing.value}',
       );
     });
     return Scaffold(
       body: Center(
-        child: Text('Count: ${count.value}\nDouble: ${doubleCount.value}'),
+        child: Text(
+          'Count: ${count.value}\nDouble: ${doubleCount.value}\n'
+          'Existing: ${existing.value}',
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => count.value++,
+        onPressed: () {
+          batch(() {
+            count.value++;
+            existing.value += 1;
+          });
+        },
         child: const Icon(Icons.add),
       ),
     );
