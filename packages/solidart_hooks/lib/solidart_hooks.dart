ed2ffba5585c683476dcf88bd8f2b37e63d175e0
Seven Du <5564821+medz@users.noreply.github.com>
2025-09-23 09:02:53 +0800

    Export SignalBuilder widget from solidart_hooks package

--- packages/solidart_hooks/lib/solidart_hooks.dart
+++ packages/solidart_hooks/lib/solidart_hooks.dart
@@ -5,3 +5,5 @@ export 'src/hooks/use_computed.dart';
 export 'src/hooks/use_effect.dart';
 export 'src/hooks/use_memoized.dart';
 export 'src/hooks/use_signal.dart';
+
+export 'src/widgets/signal_builder.dart';
