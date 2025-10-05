387e8c0db5f9746406423da2380733b807ab5778
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-05 23:18:26 +0800

    Restructure package for pub deployment
    - Move source files to lib directory
    - Add package entry point at lib/solidart.dart
    - Remove test file from src directory

---
+++ packages/solidart/lib/solidart.dart
@@ -0,0 +1,4 @@
+export 'src/core/computed.dart' show Computed;
+export 'src/core/config.dart';
+export 'src/core/effect.dart' show Effect;
+export 'src/core/signal.dart' show ReadonlySignal, Signal;
