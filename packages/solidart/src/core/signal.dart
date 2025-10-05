db891973753a33d388cdbd1d67722cd1c7e9a402
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-05 22:34:17 +0800

    Add lazy signal factory and support nullable values
    - Add Signal.lazy factory for deferred initialization
    - Make SolidartSignal support nullable types
    - Add lazy signal implementation that throws until initialized

--- packages/solidart/src/core/signal.dart
+++ packages/solidart/src/core/signal.dart
@@ -8,13 +8,16 @@ abstract interface class ReadonlySignal<T> implements Disposable {
 }
 
 abstract interface class Signal<T> implements ReadonlySignal<T> {
+  factory Signal(T initialValue, {bool? autoDispose}) = SolidartSignal<T>;
+  factory Signal.lazy({bool? autoDispose}) = SolidartLazySignal<T>;
+
   set value(T newValue);
 }
 
-class SolidartSignal<T> extends alien.PresetWritableSignal<T>
+class SolidartSignal<T> extends alien.PresetWritableSignal<T?>
     with AutoDisposable
     implements Signal<T> {
-  SolidartSignal(T initialValue, {bool? autoDispose})
+  SolidartSignal(T? initialValue, {bool? autoDispose})
       : autoDispose = autoDispose ?? SolidartConfig.autoDispose,
         super(initialValue: initialValue);
 
@@ -25,7 +28,7 @@ class SolidartSignal<T> extends alien.PresetWritableSignal<T>
   bool disposed = false;
 
   @override
-  T get value => super(null, false);
+  T get value => super(null, false) as T;
 
   @override
   void set value(T newValue) => super(newValue, true);
@@ -45,5 +48,23 @@ class SolidartSignal<T> extends alien.PresetWritableSignal<T>
   @override
   void maybeDispose() {
     if (subs == null) super.maybeDispose();
+  }
+}
+
+class SolidartLazySignal<T> extends SolidartSignal<T> {
+  SolidartLazySignal({super.autoDispose}) : super(null);
+
+  bool isInitialized = false;
+
+  @override
+  T get value {
+    if (!isInitialized) throw StateError('Signal not initialized');
+    return super.value;
+  }
+
+  @override
+  set value(T newValue) {
+    if (!isInitialized) isInitialized = true;
+    super.value = newValue;
   }
 }
