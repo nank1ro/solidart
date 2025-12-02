c762c82528761c3af6000e10d434e047ae3a7dbc
Alexandru Mariuti <alex@mariuti.com>
2025-12-02 15:40:06 +0100

    refactor: infinite scroll example and update docs

--- examples/infinite_scroll/lib/main.dart
+++ examples/infinite_scroll/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Infinite Scroll Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
