7693e13e02a35ed13a25207f8127dbdd52dd45eb
Seven Du <5564821+medz@users.noreply.github.com>
2025-12-08 18:59:18 +0800

    Add Configuration interface and global autoDispose setting
    - Add SolidartConfig with static autoDispose flag
    - Make ReadonlySignal implement Configuration
    - Add name and autoDispose parameters to Signal, LazySignal, and
      Computed
    - Export Configuration and Disposable from the main library

--- packages/solidart/lib/v3.dart
+++ packages/solidart/lib/v3.dart
@@ -1,3 +1,11 @@
 // TODO: rename the v3.dart to solidart.dart filename.
 
-export 'src/v3.dart' show Computed, Effect, LazySignal, ReadonlySignal, Signal;
+export 'src/v3.dart'
+    show
+        Computed,
+        Configuration,
+        Disposable,
+        Effect,
+        LazySignal,
+        ReadonlySignal,
+        Signal;
