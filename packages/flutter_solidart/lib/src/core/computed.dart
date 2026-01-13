f15e443106e9c9d49cbb5f96671571803e16189d
Seven Du <5564821+medz@users.noreply.github.com>
2026-01-13 22:59:54 +0800

    Align listenable signal wrappers

--- packages/flutter_solidart/lib/src/core/computed.dart
+++ packages/flutter_solidart/lib/src/core/computed.dart
@@ -1,11 +1,9 @@
-import 'package:flutter_solidart/src/core/signal.dart' show ReadonlySignal;
+import 'package:flutter/foundation.dart';
 import 'package:flutter_solidart/src/core/value_listenable_signal_mixin.dart';
 import 'package:solidart/solidart.dart' as core;
 
 /// A Solidart [core.Computed] that is also a Flutter [ValueListenable].
-class Computed<T> extends core.Computed<T>
-    with SignalValueListenableMixin<T>
-    implements ReadonlySignal<T> {
+class Computed<T> extends core.Computed<T> with SignalValueListenableMixin<T> {
   /// Creates a new [Computed] and exposes it as a [ValueListenable].
   Computed(
     super.getter, {
